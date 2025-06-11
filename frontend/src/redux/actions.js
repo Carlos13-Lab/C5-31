23e267c519e791c35f8ba0995b207e925efbd62a
Carlos Diaz <Carlosrdf45@gmai.com>
2025-06-11 16:15:44 -0400

    Update main api

--- frontend/src/redux/actions.js
+++ frontend/src/redux/actions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-     'http://localhost:5000/api/v1';
+    import.meta.env.VITE_APP_API || 'http://localhost:5000/api/v1';
 
 //=============================
 //           USER
